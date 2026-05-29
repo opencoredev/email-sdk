fc7094cdc8ca46ffd07b6697eb63ce2a616736e3
Leo <leoplayz317@gmail.com>
2026-05-29 18:36:17 -0400

    chore: add Email SDK logo

--- apps/fumadocs/src/lib/layout.shared.tsx
+++ apps/fumadocs/src/lib/layout.shared.tsx
@@ -1,12 +1,17 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
 
+import logoUrl from "../../../../logo.svg?url";
 import { appName, gitConfig } from "./shared";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     nav: {
-      // JSX supported
-      title: appName,
+      title: (
+        <span className="flex items-center gap-2">
+          <img alt="" aria-hidden="true" className="size-4 shrink-0" src={logoUrl} />
+          <span>{appName}</span>
+        </span>
+      ),
     },
     githubUrl: `https://github.com/${gitConfig.user}/${gitConfig.repo}`,
   };
